0584cfb432ce83ae37a53c770c42f4741582612e
Garrett <ga.landin89@gmail.com>
2022-03-03 12:10:45 -0600

    Created herd.py and finished methods

---
+++ herd.py
@@ -0,0 +1,10 @@
+from email.errors import HeaderDefect
+
+
+from dinosaur import Dinosaur
+class Herd
+    def __init__(self):
+        self.dinosaurs = []
+
+    def create_herd(self):
+        self.dinosaurs.append(Dinosaur)
